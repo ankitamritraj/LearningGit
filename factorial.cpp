f22b883a91dc860feb0d918ae75682ee49a90317
ankitamritraj <ankitamritraj@gmail.com>
2017-08-23 02:16:35 +0530

    initialize repository

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+#include "functions.h"
+#include<iostream>
+
+int factorial(int n){
+
+  while(n<1){
+    std::cout<<"Please enter a positive number\n";
+    std::cin>>n;
+  }
+  if(n!=1){
+    return(n * factorial(n-1));
+  }
+  else return 1;
+}
